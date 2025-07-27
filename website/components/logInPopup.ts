762d424aecc24474c3ee526336a588279a6aee0a
Dvir Arazi <108592948+DvirArazi@users.noreply.github.com>
2025-07-27 14:30:58 +0300

    fixed a prev mistake

--- website/components/logInPopup.ts
+++
@@ -1,9 +0,0 @@
-import { create, style } from "../componentUtilities";
-import { createPopup } from "./popup";
-
-export var createLogInPopup = () => {
-  return createPopup(style(
-    "/website/components/logInPopup.css",
-    create("div", { id: "titleDiv" }, ["Log In"]),
-  ));
-};
